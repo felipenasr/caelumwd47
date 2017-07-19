baf41852f7ec56e5d3786fd98320ccb034b8c1d0
felipenasr <felipenriccitrab@gmail.com>
2017-07-18 22:43:07 -0300

    terceira parte da aula, continua amanha

--- segundaAulaParte2/objetos.js
+++ segundaAulaParte2/objetos.js
@@ -1,8 +1,21 @@
 console.log('=======objetos=======');
 
 
-let carro = {};
+var carro = {};
 
-
+// PROPRIEDADES(CARACTERISTICAS)
 carro.modelo='Tucson';
 carro.ano = '2014';
+carro.fabricante = 'Hyundai';
+carro.cor = 'Prata';
+carro.ligado = false;
+
+
+// MÉTODOS (AÇÕES)
+
+carro.ligar = function () {
+  carro.ligado = true;
+}
+carro.desligar = function () {
+  carro.ligado = false;
+}
