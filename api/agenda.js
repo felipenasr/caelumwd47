73bbaf6dfbe046bed29f07cda4e2ee370b6b488c
felipenasr <felipenriccitrab@gmail.com>
2017-07-20 20:37:45 -0300

    aplicando eventos

--- api/agenda.js
+++ api/agenda.js
@@ -10,7 +10,7 @@
 
 // Declarando função anonima
 (function () {
-  // user interface
+  // user interface (interface do usuário rs)
   var ui = {
     fields: document.querySelectorAll('input'),
     button: document.querySelector('.pure-button'),
@@ -18,9 +18,17 @@
   };
   // declarando as ações do sistema!
 
-  var valideFields = function () {
-    var storeData = function () {};
-    var listAll = function () {}
-  }
+  var validateFields = function () {console.log('valida');debugger;}
+  var storeData = function () {};
+  var listAll = function () {debugger;};
+  var init = function () {
+    // maping events (mapeando os eventos do nosso app) Evento é uma ação disparada pelo usuário!
+    // quando estamos trabalhando com eventos, não devemos executar uma função assim que a relacionamos a um evento. Quem é responsável por isso é o JavaScript!
+    // ui.button.onclick = valideFields;
+
+    ui.button.addEventListener("click", validateFields);
+    ui.button.addEventListener("click", listAll);
+
+  }();
 
 })();
