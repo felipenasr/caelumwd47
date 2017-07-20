0a6d576dd8566d4314168110ae9d86dc32d6b649
felipenasr <felipenriccitrab@gmail.com>
2017-07-19 21:37:57 -0300

    segunda parte da aula

--- terceiraAulaParte3/module-pattern.js
+++ terceiraAulaParte3/module-pattern.js
@@ -1,12 +1,15 @@
-(function () {
+var MGA = (function () {
   console.log('rodando...');
   var meuGA = {};
+
   meuGA.contador = 0;
   meuGA.add = function (){
-      meuGA.contador+=1;
+      return meuGA.contador+=1;
     };
 
   meuGA.clear = function () {
     return meuGA.contador = 0;
   }
+
+  return {adiciona: meuGA.add, limpar: meuGA.clear};
 })();
