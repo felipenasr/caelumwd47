13decaef8f3779919206f847ebb5c13fc924d449
felipenasr <felipenriccitrab@gmail.com>
2017-07-20 21:58:58 -0300

    validando formulario

--- api/agenda.js
+++ api/agenda.js
@@ -18,16 +18,40 @@
   };
   // declarando as ações do sistema!
 
-  var validateFields = function () {console.log('valida');debugger;}
+  var validateFields = function (e) {
+    // preventDefault é uma função que o JS 'evita' q a ação padrão seja executada, sem que tenhamos que mudar o type do button, por exemplo
+    e.preventDefault();
+    var erros = 0;
+    var data = {};
+    ui.fields.forEach(function (field) {
+      console.log(field.value.length);
+      if(field.value.trim().length===0){
+        field.classList.add("error");
+        erros++;
+      }else{
+        field.classList.remove('error')
+        data[field.id] = field.value;
+      }
+
+      if(erros > 0){
+        document.querySelector('.error').focus();
+      }else{
+        storeData(data);
+      }
+      console.log(erros,data);
+    });
+  }
   var storeData = function () {};
-  var listAll = function () {debugger;};
+  var listAll = function () {};
+
+
   var init = function () {
     // maping events (mapeando os eventos do nosso app) Evento é uma ação disparada pelo usuário!
     // quando estamos trabalhando com eventos, não devemos executar uma função assim que a relacionamos a um evento. Quem é responsável por isso é o JavaScript!
     // ui.button.onclick = valideFields;
 
     ui.button.addEventListener("click", validateFields);
-    ui.button.addEventListener("click", listAll);
+
 
   }();
 
