75b598e8302b768d071957df6902b00eeee38e55
felipenasr <felipenriccitrab@gmail.com>
2017-07-25 20:59:03 -0300

    adicionando jquery

---
+++ cep/cep.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+  $("#cep")
+    .on('input', validateEntrey)
+    .on('focusout', getAddress);
+
+
+
+
+  var getAddress = function(){
+
+  };
+  var validateEntrey = function ()
+
+  };
+});
