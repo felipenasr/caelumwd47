1e11a41ca695ceca3a9812f81bf05c01e3da44c4
felipenasr <felipenriccitrab@gmail.com>
2017-07-25 22:46:59 -0300

    script de validação de CEP

--- cep/cep.js
+++ cep/cep.js
@@ -1,17 +1,39 @@
 $(document).ready(function() {
 
 
+  var onlyNumbers = function (e){
+    this.value = this.value.replace(/\D/g, '');
+  };
+
+  var getAddressError = function () {
+    console.error("ERRO!");
+  }
+  var getAddressSuccess = function(data){
+    $.each(data, function (key, value) {
+      $(`#${key}`).val(value);
+      console.log(key, value);
+
+    })
+  }
+
+  var getAddress = function(e){
+    var cep = this.value ;
+    if(cep.length !== 8){
+      $(this).addClass('error').focus();
+    }else{
+      $(this).removeClass('error');
+      $.ajax({
+        url: `https://viacep.com.br/ws/${cep}/json/`,
+        dataType: 'json',
+        error: getAddressError,
+        success: getAddressSuccess
+      });
+
+    }
+  };
+
   $("#cep")
-    .on('input', validateEntrey)
+    .on('input', onlyNumbers)
     .on('focusout', getAddress);
 
-
-
-
-  var getAddress = function(){
-
-  };
-  var validateEntrey = function ()
-
-  };
 });
