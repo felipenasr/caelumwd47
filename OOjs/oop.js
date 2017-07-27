e22902e228a21e1f217209654cd810b671252a53
felipenasr <felipenriccitrab@gmail.com>
2017-07-26 22:30:06 -0300

    OOjs

---
+++ OOjs/oop.js
@@ -0,0 +1,25 @@
+var contaZe = new Conta();
+
+
+function Conta (numero, tipo, saldo){
+  this.numero = numero||0;
+  this.tipo = tipo||null;
+  this.saldo = saldo||0;
+}
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  return this.saldo
+};
+Conta.prototype.extrato = function () {
+  return this.saldo;
+};
+Conta.prototype.sacar = function (valor) {
+  return this.saldo -= valor  ;
+};
+Conta.prototype.transfere = function (valor, contaDestino) {
+  this.sacar(valor);
+  contaDestino.depositar(valor);
+};
+
+
+console.log(contaZe);
